a89d07a42e4545532201c7830dd79a8179114293
puspawahyuningtias <puspa.wahyuningtias@gmail.com>
2024-04-05 18:03:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from "./Pages";
+import Login from "./Pages/login";
 
 function App() {
   return (
     <div>
       <Routes>
         {/* Auth */}
-        <Route name='Home' path="/" element={<Home />} />
+        <Route name='Login' path="/" element={<Login />} />
+        <Route name='Home' path="/home" element={<Home />} />
       </Routes>
     </div>
   );

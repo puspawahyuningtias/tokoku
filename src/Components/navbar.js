937a2d7512dc56a95a73d0fc9e3d3bea509d3000
puspawahyuningtias <puspa.wahyuningtias@gmail.com>
2024-04-05 20:52:30 +0700

    Update navbar.js

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -10,7 +10,7 @@ function Navbar({ SubNavbar, NameSubNavbar, LinkBack }) {
                 </div>
                 <div className={`${SubNavbar ? 'block' : 'hidden'} flex items-center gap-[16px]`}>
                     <Link to={LinkBack}>
-                        {/* <BsArrowLeftShort className='text-[#DC1717] text-2xl' /> */}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.78em" height="1em" viewBox="0 0 16 9"><path fill="#C1121F" d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5" /><path fill="#C1121F" d="M6 8.5a.47.47 0 0 1-.35-.15l-3.5-3.5c-.2-.2-.2-.51 0-.71L5.65.65c.2-.2.51-.2.71 0c.2.2.2.51 0 .71L3.21 4.51l3.15 3.15c.2.2.2.51 0 .71c-.1.1-.23.15-.35.15Z" /></svg>
                     </Link>
                     <h1 className='text-[#DC1717] text-sm font-[500]'>{NameSubNavbar}</h1>
                 </div>
